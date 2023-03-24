bee6a70db65a711838d842202fff2b4e8d472dee
Saul Florez <sflorez@globaltask.net>
2023-03-23 22:52:18 -0400

    📦 NEW: Animación de abrir menú

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import './src/sass/main.scss';
 
 function main() {
-  console.log('Integramos Vite!')
+  const buttonHeader = document.querySelector(".header__button")
+  const menuElement = document.querySelector(".menu")
+  buttonHeader.addEventListener("click", () => {
+    document.body.style.overflow = "hidden"
+    menuElement.style.visibility = "visible"
+    menuElement.style.opacity = 1
+    menuElement.classList.add("menu--show");
+  })
+
+  const closeButtonHeader = document.querySelector(".menu__button-close")
+  closeButtonHeader.addEventListener("click", () => {
+    document.body.style.overflow = "inherit"
+    menuElement.classList.remove("menu--show");
+  })
 }
 
 main()
